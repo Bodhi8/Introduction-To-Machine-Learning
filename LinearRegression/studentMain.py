12d056d83d2d118c8837c0c2ca7b036e01a9cd42
Menfi Systems <Menfi@Michaels-Mac-mini.local>
2017-03-15 05:54:44 -0500

    Introduction To Machine Learning - Linear Regression 47 of 47

--- LinearRegression/studentMain.py
+++ LinearRegression/studentMain.py
@@ -4,7 +4,6 @@ Created on Mar 14, 2017
 @author: Menfi
 '''
 
-
 import matplotlib.pyplot as plt
 from studentRegression import studentReg
 from class_vis import prettyPicture, output_image
@@ -22,7 +21,6 @@ print(ages_train[0:2,])
 print("\ntype(ages_train) - {}".format(type(ages_train)))
 #        type(ages_train) - <class 'numpy.ndarray'>
 
-
 print("\nnet_worths_train[0:2,0:3] - {}".format(net_worths_train[0:2,0:3]))
 print("\nnet_worths_train[0:2] - ")
 print(net_worths_train[0:2,])
@@ -30,11 +28,14 @@ print(net_worths_train[0:2,])
 print("type(net_worths_train) - {}\n".format(type(net_worths_train)))
 #      type(net_worths_train) - <class 'numpy.ndarray'>
 
-
-
 # Call the studentRegression.py function studentReg passing ages_train, net_worths_train
 reg = studentReg(ages_train, net_worths_train)
 
+print("reg.coef_ - {}".format(reg.coef_))
+#      reg.coef_ - [[ 6.30945055]] - slope
+
+print("type(reg.coef_) - {}\n".format(type(reg.coef_)))
+#      type(reg.coef_) - <class 'numpy.ndarray'> - slope
 
 plt.clf()
 plt.scatter(ages_train, net_worths_train, color="b", label="train data")
@@ -44,7 +45,6 @@ plt.legend(loc=2) # did not seem to make a difference
 plt.xlabel("ages")
 plt.ylabel("net worths")
 plt.show()
-
 
 plt.savefig("test.png")
 output_image("test.png", "png", open("test.png", "rb").read())
